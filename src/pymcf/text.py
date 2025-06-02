5dfee107a2913edadf770e2d4fd1db5e0ada33e5
00ll00 <o0ll00@qq.com>
2025-06-02 11:35:34 +0800

    *

--- src/pymcf/text.py
+++ src/pymcf/text.py
@@ -13,7 +13,7 @@ def text_component(data: Any, /, **style) -> TextComponent:
     elif isinstance(data, Entity):
         comp = TextSelectorCompoment(data.__metadata__)
     elif isinstance(data, list | tuple):
-        comp = TextComponentHolder(style=None, children=[text_component(d) for d in data])
+        comp = TextComponentHolder(style={}, children=[text_component(d) for d in data])
     else:
         comp = TextStringComponent(str(data))
     if style:
