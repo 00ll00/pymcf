fc1713c41f2d3c50327d32d30c1bcb2d5c05eecb
00ll00 <o0ll00@qq.com>
2025-08-17 17:59:02 +0800

    *

--- src/pymcf/nbtlib.py
+++ src/pymcf/nbtlib.py
@@ -2,15 +2,50 @@
 Wrapper of nbtlib
 """
 
-from nbtlib import Byte as NbtByte, ByteArray as NbtByteArray, Short as NbtShort, Int as NbtInt, \
-    IntArray as NbtIntArray, \
-    Long as NbtLong, LongArray as NbtLongArray, Float as NbtFloat, Double as NbtDouble, Compound as NbtCompound, \
-    List as NbtList, \
-    Array as NbtArray, String as NbtString
+from nbtlib import Byte as _Byte, ByteArray as _ByteArray, Short as _Short, Int as _Int, \
+    IntArray as _IntArray, \
+    Long as _Long, LongArray as _LongArray, Float as _Float, Double as _Double, Compound as _Compound, \
+    List as _List, Array as _Array, String as _String, Base as _Base
 
 from nbtlib import Path as _NbtPath
 
 from nbtlib.schema import CompoundSchema as NbtCompoundSchema
+from pymcf.ast_ import Resolvable
+
+
+class _NbtResolvable(_Base, Resolvable):
+
+    def resolve(self, scope):
+        return self.snbt(compact=True,quote="'")
+
+
+class NbtByte(_Byte, _NbtResolvable):
+    pass
+class NbtByteArray(_ByteArray, _NbtResolvable):
+    pass
+class NbtShort(_Short, _NbtResolvable):
+    pass
+class NbtInt(_Int, _NbtResolvable):
+    pass
+class NbtIntArray(_IntArray, _NbtResolvable):
+    pass
+class NbtLong(_Long, _NbtResolvable):
+    pass
+class NbtLongArray(_LongArray, _NbtResolvable):
+    pass
+class NbtFloat(_Float, _NbtResolvable):
+    pass
+class NbtDouble(_Double, _NbtResolvable):
+    pass
+class NbtCompound(_Compound, _NbtResolvable):
+    pass
+class NbtList(_List, _NbtResolvable):
+    pass
+class NbtArray(_Array, _NbtResolvable):
+    pass
+class NbtString(_String, _NbtResolvable):
+    pass
+
 
 NbtData = NbtByte | NbtByteArray | NbtShort | NbtInt | NbtIntArray | NbtLong | NbtLongArray | NbtFloat | \
           NbtDouble | NbtCompound | NbtList | NbtArray | NbtString
